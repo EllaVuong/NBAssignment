c0fe00d83ee21ec71293cc42744f2750bc3abbcf
EllaVuong <73335050+EllaVuong@users.noreply.github.com>
2024-11-18 12:52:58 +0100

    added logic and helper methods to check if the date is toll free

--- NBAssignment/Program.cs
+++ NBAssignment/Program.cs
@@ -8,19 +8,46 @@ class program
 	{
 		Vehicle car = new Car();
 		Vehicle	motorbike = new Motorbike();
+		Vehicle diplomat = new Diplomat();
+		Vehicle em = new Emergency();
+		Vehicle military = new Military();
+		Vehicle tractor = new Tractor();
 
 		TollCalculator tollCalculator = new TollCalculator();
 			DateTime[] passes = {
 			//new DateTime(2024, 11, 15, 5, 0, 0), // 0 kr
 			new DateTime(2024, 11, 15, 6, 0, 0), // 8 kr
 			new DateTime(2024, 11, 15, 6, 15, 0), // 8 kr
-            new DateTime(2024, 11, 15, 6, 30, 0), // 13 kr
-            new DateTime(2024, 11, 15, 7, 30, 0),  // 18 kr
-			new DateTime(2024, 12, 24, 6, 0, 0) // christmas eve
+			new DateTime(2024, 11, 15, 6, 30, 0), // 13 kr
+			new DateTime(2024, 11, 15, 7, 30, 0),  // 18 kr
+
+			//Holidays
+			//new datetime(2024, 1, 1), //new year's day 
+			//new datetime(2024, 1, 6), //epiphany
+			//new datetime(2024, 5, 1), //1 may
+			//new datetime(2024, 6, 1), //national day 
+			//new datetime(2024, 12, 24), //christmas eve
+			//new datetime(2024, 12, 25), //christmas day           
+			//new datetime(2024, 12, 26), //boxing day
+			//new datetime(2024, 12, 31), //new year eve
+			//new datetime(2024, 3, 29), //easter friday
+			//new datetime(2024, 3, 30), //easter eve
+			//new datetime(2024, 3, 31), //easter day         
+			//new datetime(2024, 4, 1), //easter monday
+			//new datetime(2024, 5, 19), //whitsun
+			//new datetime(2024, 6, 21), //midsummer eve
+			//new datetime(2024, 6, 22), //midsummer day
+			//new datetime(2024, 11, 2), //all saints day
+			//new datetime(2024, 3, 28), //day before easter
+			//new datetime(2024, 11, 1) //day before all saints day
 		};
 
 		Console.WriteLine($"Car Toll: {tollCalculator.GetTollFee(car, passes)}"); //31
 		Console.WriteLine($"Motorbike Toll: {tollCalculator.GetTollFee(motorbike, passes)}"); //0
+		Console.WriteLine($"Diplomat Toll: {tollCalculator.GetTollFee(diplomat, passes)}"); //0
+		Console.WriteLine($"Emergency Toll: {tollCalculator.GetTollFee(em, passes)}"); //0
+		Console.WriteLine($"Military Toll: {tollCalculator.GetTollFee(military, passes)}"); //0
+		Console.WriteLine($"Tractor Toll: {tollCalculator.GetTollFee(tractor, passes)}"); //0
 	}
 }
 
--- NBAssignment/TollCalculator.cs
+++ NBAssignment/TollCalculator.cs
@@ -14,7 +14,7 @@ public class TollCalculator
      */
 
     const int MAX_DAILY_FEE = 60;
-    const int TOLL_FREE_WINDOW = 60;
+    const int TOLL_FREE_WINDOW_FOR_THE_DAY = 60;
 
     public int GetTollFee(Vehicle vehicle, DateTime[] dates)
     {
@@ -25,27 +25,20 @@ public class TollCalculator
 		foreach (DateTime vehiclePass in dates)
 		{
 			int currentFee = GetTollFee(vehiclePass, vehicle);
-
-			//long diffInMillies = date.Millisecond - intervalStart.Millisecond;
-			//long minutes = diffInMillies / 1000 / 60;
 			TimeSpan timeDiff = vehiclePass - intervalStart;
             long minutes = (long)timeDiff.TotalMinutes;
 
-			if (minutes <= TOLL_FREE_WINDOW)
-			{
+			if (minutes <= TOLL_FREE_WINDOW_FOR_THE_DAY) {
 				if (totalFee > 0) 
                     totalFee -= intervalFee;
 				if (currentFee >= intervalFee) 
                     intervalFee = currentFee;
 				totalFee += intervalFee;
 			}
-			else
-			{
+			else {
 				totalFee += currentFee;
 			}
 		}
-		//if (totalFee > MAX_DAILY_FEE) totalFee = MAX_DAILY_FEE;
-		//return totalFee;
 		return Math.Min(totalFee, MAX_DAILY_FEE);
 	}
 
@@ -84,48 +77,118 @@ public class TollCalculator
 
 	private Boolean IsTollFreeDate(DateTime date)
     {
-        //int year = date.Year;
-        //int month = date.Month;
-        //int day = date.Day;
-
         if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) 
             return true;
 
         if (date.Month == 7) //July
             return true;
 
+        if (IsDayBeforeHoliday(date))
+            return true;
+
         return IsPublicHoliday(date);
-        //if (year == 2013)
-        //{
-        //    if (month == 1 && day == 1 ||
-        //        month == 3 && (day == 28 || day == 29) ||
-        //        month == 4 && (day == 1 || day == 30) ||
-        //        month == 5 && (day == 1 || day == 8 || day == 9) ||
-        //        month == 6 && (day == 5 || day == 6 || day == 21) ||
-        //        month == 7 ||
-        //        month == 11 && day == 1 ||
-        //        month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
-        //    {
-        //        return true;
-        //    }
-        //}
-        //return false;
     }
 
     private Boolean IsPublicHoliday(DateTime date)
     {
+        var publicHoliday = GetPublicHolidays(date.Year);
+        return publicHoliday.Contains(date.Date);
+    }
+
+    private List<DateTime> GetPublicHolidays(int year)
+    {
         var publicHolidays = new List<DateTime>
         {
-            new DateTime(date.Year, 1, 1), //New Year 
-            new DateTime(date.Year, 1, 6), //Thirteenth day of Christmas
-            new DateTime(date.Year, 1, 5), // 1 May
-            new DateTime(date.Year, 6, 1), // National day 
-            new DateTime(date.Year, 12, 24), //Christmas eve
-            new DateTime(date.Year, 12, 25), //Christmas day           
-            new DateTime(date.Year, 12, 26), //Boxing day
-            new DateTime(date.Year, 12, 31) //New Year eve
+            new DateTime(year, 1, 1), //New Year's day 
+            new DateTime(year, 1, 6), //Epiphany
+            new DateTime(year, 5, 1), //1 May
+            new DateTime(year, 6, 1), //National day 
+            new DateTime(year, 12, 24), //Christmas eve
+            new DateTime(year, 12, 25), //Christmas day           
+            new DateTime(year, 12, 26), //Boxing day
+            new DateTime(year, 12, 31) //New Year eve
 		};
-        return publicHolidays.Contains(date.Date);
+
+        //Easter
+        DateTime goodFriday = GetEasterSunday(year).AddDays(-2);
+        DateTime easterEve = GetEasterSunday(year).AddDays(-1);
+        DateTime easterMonday = GetEasterSunday(year).AddDays(1);
+        publicHolidays.Add(goodFriday);
+        publicHolidays.Add(easterEve);
+        publicHolidays.Add(easterMonday);
+        publicHolidays.Add(GetEasterSunday(year));
+
+        //Ascension Day/ Kristi Himmelfärds dag
+        //6th thursday/ 40 days after easter sunday
+        DateTime ascensionDay = GetEasterSunday(year).AddDays(39);
+        publicHolidays.Add(ascensionDay);
+
+        //Whitsun/Pingstdagen
+        //7 weeks after Easter Sunday
+        DateTime whitSunday = GetEasterSunday(year).AddDays(49);
+        publicHolidays.Add(whitSunday);
+
+        //Midsummer eve + Midsummer day
+        DateTime midsummerEve = GetMidsummerDay(year).AddDays(-1);
+        publicHolidays.Add(midsummerEve);
+		publicHolidays.Add(GetMidsummerDay(year));
+
+        //All Saints day
+        publicHolidays.Add(GetAllSaintsDay(year));
+
+		return publicHolidays;
+    }
+
+    private DateTime GetEasterSunday(int year)
+    {
+		int a = year % 19;
+		int b = year / 100;
+		int c = year % 100;
+		int d = b / 4;
+		int e = b % 4;
+		int f = (b + 8) / 25;
+		int g = (b - f + 1) / 3;
+		int h = (19 * a + b - d - g + 15) % 30;
+		int i = c / 4;
+		int k = c % 4;
+		int l = (32 + 2 * e + 2 * i - h - k) % 7;
+		int m = (a + 11 * h + 22 * l) / 451;
+		int month = (h + l - 7 * m + 114) / 31;
+		int day = ((h + l - 7 * m + 114) % 31) + 1;
+
+		return new DateTime(year, month, day);
+	}
+
+    private DateTime GetMidsummerDay(int year)
+    {
+		//the saturday between 20 june - 26 june
+		DateTime june20 = new DateTime(year, 6, 20);
+        for (int i = 0; i <= 6; i++) { 
+            if (june20.AddDays(i).DayOfWeek == DayOfWeek.Saturday)
+                return june20.AddDays(i);
+        }
+        throw new InvalidOperationException("Cannot calculate the Midsummer Day.");
+    }
+
+    private DateTime GetAllSaintsDay(int year)
+    {
+		//the saturday between 31 oct - 5 nov
+		DateTime oct31 = new DateTime(year, 10, 31);
+        for (int i = 0; i <= 6; i++) {
+            if (oct31.AddDays(i).DayOfWeek == DayOfWeek.Saturday)
+                return oct31.AddDays(i);
+        }
+        throw new InvalidOperationException("Cannont calculate the All Saints Day.");
+    }
+
+    private Boolean IsDayBeforeHoliday(DateTime date)
+    {
+        var publicHolidays = GetPublicHolidays(date.Year);
+        foreach (var holiday in publicHolidays) {
+            if (holiday.AddDays(-1) == date.Date)
+                return true;
+        }
+        return false;
     }
 
     private enum TollFreeVehicles
